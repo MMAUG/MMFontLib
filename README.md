ebc589c95642eb5b3dc0027d28dd6eb03caf7fc3
Ye Lin Aung <me@yelinaung.com>
2014-07-18 03:33:35 +0630

    Add LICENSE

--- README.md
+++ README.md
@@ -25,9 +25,38 @@ dependencies {
 Replace your `TextView` with embedded view.
 
 ```xml
-      <org.mmodroid.mmfont.MM3
-          android:id="@+id/tv_mm3"
-          android:layout_width="wrap_content"
-          android:layout_height="wrap_content"
-          android:textSize="25sp" />
+<org.mmodroid.mmfont.MM3
+    android:id="@+id/tv_mm3"
+    android:layout_width="wrap_content"
+    android:layout_height="wrap_content"
+    android:textSize="25sp" />
 ```
+
+Contributing
+------------
+
+ 1. Fork it
+ 2. Create your feature branch (`git checkout -b my-new-feature`)
+ 3. Commit your changes (`git commit -am 'Added some feature'`)
+ 4. Push to the branch (`git push origin my-new-feature`)
+ 5. Create new Pull Request
+
+License
+--------
+
+    Copyright 2014 Myanmar Android User Group(MMAUG)
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+
+
+Fonts are respectively copyrighted by font owners.
--- lib/src/main/java/org/mmaug/mmfont/MyMM.java
+++ lib/src/main/java/org/mmaug/mmfont/MyMM.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2014 Myanmar Android User Group(MMAUG)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.mmaug.mmfont;
 
 import android.content.Context;
@@ -7,23 +23,22 @@ import android.widget.TextView;
 
 public class MyMM extends TextView {
 
-	public MyMM(Context context) {
-		super(context);
-		setStyle(context);
-	}
+  public MyMM(Context context) {
+    super(context);
+    setStyle(context);
+  }
 
-	public MyMM(Context context, AttributeSet attrs) {
-		super(context, attrs);
-		setStyle(context);
-	}
+  public MyMM(Context context, AttributeSet attrs) {
+    super(context, attrs);
+    setStyle(context);
+  }
 
-	public MyMM(Context context, AttributeSet attrs, int defStyle) {
-		super(context, attrs, defStyle);
-		setStyle(context);
-	}
+  public MyMM(Context context, AttributeSet attrs, int defStyle) {
+    super(context, attrs, defStyle);
+    setStyle(context);
+  }
 
-	private void setStyle(Context context) {
-		setTypeface(Typeface.createFromAsset(context.getAssets(),
-				"fonts/MyMM.ttf"));
-	}
+  private void setStyle(Context context) {
+    setTypeface(Typeface.createFromAsset(context.getAssets(), "fonts/MyMM.ttf"));
+  }
 }
