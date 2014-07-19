385361537773288c7e24d1519de90238b791e7bb
Ye Lin Aung <me@yelinaung.com>
2014-07-19 17:09:32 +0630

    Add NotoSans and Noto class

---
+++ lib/src/main/java/org/mmaug/mmfont/Noto.java
@@ -0,0 +1,46 @@
+/*
+ * Copyright 2014 Myanmar Android User Group(MMAUG)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.mmaug.mmfont;
+
+import android.content.Context;
+import android.graphics.Typeface;
+import android.util.AttributeSet;
+import android.widget.TextView;
+
+/**
+ * Created by Ye Lin Aung on 14/07/19.
+ */
+public class Noto extends TextView {
+  public Noto(Context context) {
+    super(context);
+    setStyle(context);
+  }
+
+  public Noto(Context context, AttributeSet attrs) {
+    super(context, attrs);
+    setStyle(context);
+  }
+
+  public Noto(Context context, AttributeSet attrs, int defStyle) {
+    super(context, attrs, defStyle);
+    setStyle(context);
+  }
+
+  private void setStyle(Context context) {
+    setTypeface(Typeface.createFromAsset(context.getAssets(), "fonts/NotoSansMyanmar-Regular.ttf"));
+  }
+}
