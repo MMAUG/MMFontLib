db8c5fc9fc7c1b7377bb6b0b3035811f8afa48c7
Ye Lin Aung <me@yelinaung.com>
2014-09-05 10:35:14 +0630

    experimental FontUtils

--- library/src/main/java/org/mmaug/mmfont/FontUtils.java
+++ library/src/main/java/org/mmaug/mmfont/FontUtils.java
@@ -4,14 +4,31 @@ import android.content.Context;
 import android.widget.TextView;
 
 /**
- * Created by Ye Lin Aung on 14/09/04.
+ * TextView တစ်ခု (သို့) အများအပြားကို Typeface set လုပ်ပေးမည့် Utility methods များ
+ *
+ * @author Ye Lin Aung me@yelinaung.com
  */
 public class FontUtils {
+
+  /**
+   * TextView တစ်ခုကို Typeface set လုပ်ခြင်း
+   *
+   * @param fontName embed လုပ်မည့် font အမည်
+   * @param context ဖောင့်များကို {@link FontCache} မှ တဆင့် load လုပ်ရန်အတွက် လိုအပ်သည့် Context
+   * @param textView Typeface set လုပ်မည့် TextView
+   */
 
   public static void setTypeFace(String fontName, Context context, TextView textView) {
     textView.setTypeface(FontCache.get(fontName, context));
   }
 
+  /**
+   * TextViews များကို Typeface set လုပ်ခြင်း
+   *
+   * @param fontName embed လုပ်မည့် font အမည်
+   * @param context ဖောင့်များကို {@link FontCache} မှ တဆင့် load လုပ်ရန်အတွက် လိုအပ်သည့် Context
+   * @param textViews Typeface set လုပ်မည့် TextView များ
+   */
   public static void setTypeFace(String fontName, Context context, TextView... textViews) {
     for (TextView t : textViews) {
       t.setTypeface(FontCache.get(fontName, context));
